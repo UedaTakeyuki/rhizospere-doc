d6177a58bbe8fd35fe432e2b8fae0924dd637678
Takeyuki UEDA <ueda@latelierdueda.com>
2021-01-27 22:23:41 +0900

    nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Koshinto documents',
-  description: 'Prevent casual tampering casually.',
+  title: 'Rhizospere documents',
+  description: 'Provides a way to control your Raspberry Pi working somewhere in the world.',
   dest: 'public/docs',
   base: '/docs/',
   markdown: {
@@ -9,4 +9,25 @@ module.exports = {
       md.use(require('markdown-it-footnote'))
     }
   },
+  plugins: [
+    [ 
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-43410269-6' // UA-00000000-0
+      }
+    ],
+    [
+      'vuepress-plugin-google-adsense',
+      {
+        'google_ad_client': 'ca-pub-1717720203982550', // ca-pub-0000000000000000
+        'enable_page_level_ads': true
+      }
+    ],
+    ['mermaidjs']
+  ],
+  themeConfig: {
+    nav: [
+      {text: 'back to App', link: '/'}
+    ]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,3 @@
 # Hello rhizospere
+
+Gives a efficient way to connect and control your Raspberry Pi working somewhere in the world.
